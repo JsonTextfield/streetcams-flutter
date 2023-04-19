16a8b86e935a0edb261f4c03404ef0bcf5e6a551
Jason <tehblackboy@gmail.com>
2023-04-19 10:50:53 -0400

    added dismissible background widgets and updated translation for Search by neighbourhood

--- lib/widgets/camera_list_view.dart
+++ lib/widgets/camera_list_view.dart
@@ -67,6 +67,26 @@ class CameraListView extends StatelessWidget {
             return Dismissible(
               key: UniqueKey(),
               onDismissed: (direction) => dismissed(),
+              background: Container(
+                color: Constants.accentColour,
+                padding: const EdgeInsets.only(left: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Icon(
+                    camera.isVisible ? Icons.visibility_off : Icons.visibility,
+                  ),
+                ),
+              ),
+              secondaryBackground: Container(
+                color: Constants.accentColour,
+                padding: const EdgeInsets.only(right: 20),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Icon(
+                    camera.isVisible ? Icons.visibility_off : Icons.visibility,
+                  ),
+                ),
+              ),
               child: ListTile(
                 selected: state.selectedCameras.contains(camera),
                 selectedTileColor: Constants.accentColour,
