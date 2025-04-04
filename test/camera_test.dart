9bc27605beff5bfb429d3fe71755e59d4571346c
jason <tehblackboy@gmail.com>
2025-04-03 22:20:21 -0400

    updated tests

--- test/camera_test.dart
+++ test/camera_test.dart
@@ -1,57 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:streetcams_flutter/entities/bilingual_object.dart';
 import 'package:streetcams_flutter/entities/camera.dart';
 import 'package:streetcams_flutter/entities/city.dart';
-import 'package:streetcams_flutter/entities/latlon.dart';
 
 void main() {
-  test('test equality', () {
-    String nameEn = 'CameraEn';
-    String nameFr = 'CameraFr';
-    LatLon location = const LatLon(lat: 45.454545, lon: -75.696969);
-    City city = City.ottawa;
-
-    var camera1 = Camera(
-        city: city,
-        location: location,
-        name: BilingualObject(en: nameEn, fr: nameFr));
-    var camera2 = Camera(
-        city: city,
-        location: location,
-        name: BilingualObject(en: nameEn, fr: nameFr));
-
-    expect(camera1, camera2);
-  });
-
-  test('test equality with non-equatable fields', () {
-    String nameEn = 'CameraEn';
-    String nameFr = 'CameraFr';
-    LatLon location = const LatLon(lat: 45.454545, lon: -75.696969);
-    City city = City.ottawa;
-
-    var camera1 = Camera(
-      city: city,
-      location: location,
-      name: BilingualObject(en: nameEn, fr: nameFr),
-      neighbourhood: const BilingualObject(en: 'Riverdale'),
-      url: 'test.url',
-    )
-      ..isVisible = true
-      ..isFavourite = false;
-
-    var camera2 = Camera(
-      city: city,
-      location: location,
-      name: BilingualObject(en: nameEn, fr: nameFr),
-      neighbourhood: const BilingualObject(en: 'Downtown'),
-      url: 'url.test',
-    )
-      ..isVisible = false
-      ..isFavourite = true;
-
-    expect(camera1, camera2);
-  });
-
   test('camera creation city from json', () {
     var camera = Camera.fromJson(<String, dynamic>{'city': 'ottawa'});
     expect(camera.city, City.ottawa);
--- test/fake_local_storage_data_source.dart
+++ test/fake_local_storage_data_source.dart
@@ -1,35 +1,61 @@
+import 'package:flutter/src/material/app.dart';
+import 'package:streetcams_flutter/blocs/camera_state.dart';
 import 'package:streetcams_flutter/data/local_storage_data_source.dart';
+import 'package:streetcams_flutter/data/shared_preferences_data_source.dart';
+import 'package:streetcams_flutter/entities/city.dart';
 
-class FakeLocalStorageDataSource implements ILocalStorageDataSource {
+class FakeLocalStorageDataSource implements IPreferencesDataSource {
   final Map<String, Object> map = {};
 
   @override
-  Future<bool?> getBool(String key) async {
-    return map[key] as bool?;
+  Future<void> favourite(List<String> ids, bool value) async {
+    map['favourites'] =
+        value ? map['favourites'] ?? [] + ids : map['favourites'] ?? [] - ids;
   }
 
   @override
-  void setBool(String key, bool value) {
-    map[key] = value;
+  Future<List<String>> getFavourites() async {
+    return (map['favourites'] ?? <String>[]) as List<String>;
   }
 
   @override
-  Future<int?> getInt(String key) async {
-    return map[key] as int?;
+  Future<void> setVisibility(List<String> ids, bool value) async {
+    map['hidden'] =
+        value ? map['hidden'] ?? [] + ids : map['hidden'] ?? [] - ids;
   }
 
   @override
-  void setInt(String key, int value) {
-    map[key] = value;
+  Future<List<String>> getHidden() async {
+    return (map['hidden'] ?? <String>[]) as List<String>;
   }
 
   @override
-  Future<String?> getString(String key) async {
-    return map[key] as String?;
+  Future<City> getCity() async {
+    return (map['city'] ?? City.ottawa) as City;
   }
 
   @override
-  void setString(String key, String value) {
-    map[key] = value;
+  Future<ThemeMode> getTheme() async {
+    return (map['theme'] ?? ThemeMode.system) as ThemeMode;
+  }
+
+  @override
+  Future<ViewMode> getViewMode() async {
+    return (map['viewMode'] ?? ViewMode.list) as ViewMode;
+  }
+
+  @override
+  Future<void> setCity(City city) async {
+    map['city'] = city;
+  }
+
+  @override
+  Future<void> setTheme(ThemeMode theme) async {
+    map['theme'] = theme;
+  }
+
+  @override
+  Future<void> setViewMode(ViewMode viewMode) async {
+    map['viewMode'] = viewMode;
   }
 }
